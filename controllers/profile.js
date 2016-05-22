b25d9758ae3c649662ee67f1d4d0a7fb028f2c0f
Will Schumacher <meintodengel@gmail.com>
2016-05-22 11:56:47 -0700

    unmatch members from profile view

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,9 +1,9 @@
 angular.module('app')
   .controller('Profile', Profile)
 
-Profile.$inject = ['User', 'user', 'matches'];
+Profile.$inject = ['User', 'Members', 'user', 'matches'];
 
-function Profile(User, user, matches) {
+function Profile(User, Members, user, matches) {
   const vm = this;
 
   vm.matches = [];
@@ -13,6 +13,19 @@ function Profile(User, user, matches) {
   }
 
   vm.user = user;
+
+  vm.unmatch = function(id) {
+    for (i in matches) {
+      if (vm.matches[i]._id === id) {
+        vm.matches.splice(i, 1);
+        break;
+      }
+    }
+    Members.match(user, id).then(data => {
+      User.updateUser(data.data.data);
+    })
+  }
+
   console.log(vm.user);
   console.log(vm.matches);
 }
